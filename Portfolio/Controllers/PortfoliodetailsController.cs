7ff64478fb103efd1521d074f0e27f8b22e98d3d
aminkhaled27 <amin.kh.27@gmail.com>
2025-04-14 14:55:34 +0200

    MVC-based portfolio showcases my projects with a modular design, using Models to manage data, Views for a clean UI, and Controllers for seamless user interaction.

---
+++ Portfolio/Controllers/PortfoliodetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class PortfoliodetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
